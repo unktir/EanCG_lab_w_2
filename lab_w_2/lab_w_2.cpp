cc7f970eaaefde2bf935f3cd3871973708d1caab
unktir <valera47304@gmail.com>
2022-04-15 16:57:57 +0500

    Step 3

--- lab_w_2/lab_w_2.cpp
+++ lab_w_2/lab_w_2.cpp
@@ -7,9 +7,10 @@ GLuint VBO;
 float Scale = 0.0f;
 
 /*	Объявление функций	*/
-static void CreateVertexBuffer(glm::mat4& m, glm::mat4& r);
+static void CreateVertexBuffer(glm::mat4& m, glm::mat4& r, glm::mat4& c);
 glm::mat4 moving(glm::mat4& m);
 glm::mat4 rotation(glm::mat4& m);
+glm::mat4 conversion(glm::mat4& mtrx);
 
 /*	Функция рендера сцены	*/
 static void RenderScene()
@@ -20,8 +21,9 @@ static void RenderScene()
 
 	glm::mat4 mov_mtrx = moving(mov_mtrx);
 	glm::mat4 rot_mtrx = rotation(rot_mtrx);
+	glm::mat4 con_mtrx = conversion(con_mtrx);
 
-	CreateVertexBuffer(mov_mtrx, rot_mtrx);
+	CreateVertexBuffer(mov_mtrx, rot_mtrx, con_mtrx);
 
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -35,7 +37,7 @@ static void RenderScene()
 }
 
 /*	Функция создания буфера вершин	*/
-static void CreateVertexBuffer(glm::mat4& m, glm::mat4& r)
+static void CreateVertexBuffer(glm::mat4& m, glm::mat4& r, glm::mat4& c)
 {
 	/*	Задаём начальные координаты	*/
 	glm::vec3 Vertices[3];
@@ -44,9 +46,9 @@ static void CreateVertexBuffer(glm::mat4& m, glm::mat4& r)
 	Vertices[2] = glm::vec3(0.0f, 1.0f, 0.0f);
 
 	/*	Задаёт координаты перемещения	*/
-	Vertices[0] = glm::vec4(Vertices[0], 1.0f) * m * r;
-	Vertices[1] = glm::vec4(Vertices[1], 1.0f) * m * r;
-	Vertices[2] = glm::vec4(Vertices[2], 1.0f) * m * r;
+	Vertices[0] = glm::vec4(Vertices[0], 1.0f) * m * r * c;
+	Vertices[1] = glm::vec4(Vertices[1], 1.0f) * m * r * c;
+	Vertices[2] = glm::vec4(Vertices[2], 1.0f) * m * r * c;
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -71,6 +73,17 @@ glm::mat4 rotation(glm::mat4& mtrx)
 	mtrx[1][0] = sinf(Scale); mtrx[1][1] = cosf(Scale);  mtrx[1][2] = 0.0f; mtrx[1][3] = 0.0f;
 	mtrx[2][0] = 0.0f;        mtrx[2][1] = 0.0f;         mtrx[2][2] = 1.0f; mtrx[2][3] = 0.0f;
 	mtrx[3][0] = 0.0f;        mtrx[3][1] = 0.0f;         mtrx[3][2] = 0.0f; mtrx[3][3] = 1.0f;
+
+	return mtrx;
+}
+
+/*	Функция маштабирования	*/
+glm::mat4 conversion(glm::mat4& mtrx)
+{
+	mtrx[0][0] = sinf(Scale); mtrx[0][1] = 0.0f;        mtrx[0][2] = 0.0f;        mtrx[0][3] = 0.0f;
+	mtrx[1][0] = 0.0f;        mtrx[1][1] = cosf(Scale); mtrx[1][2] = 0.0f;        mtrx[1][3] = 0.0f;
+	mtrx[2][0] = 0.0f;        mtrx[2][1] = 0.0f;        mtrx[2][2] = sinf(Scale); mtrx[2][3] = 0.0f;
+	mtrx[3][0] = 0.0f;        mtrx[3][1] = 0.0f;        mtrx[3][2] = 0.0f;        mtrx[3][3] = 1.0f;
 
 	return mtrx;
 }
